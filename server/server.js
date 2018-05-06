1f7db7ea9f6ec725404c066289dca50ba0705a80
Prakhar Shukla <prakshukla@deloitte.com>
2018-05-07 01:28:11 +0530

    adding test cases

--- server/server.js
+++ server/server.js
@@ -22,3 +22,5 @@ app.post("/todos", (req, res) => {
 app.listen(3000, () => {
     console.log("Listening to port 3000");
 });
+
+module.exports = {app};
---
+++ server/tests/server.test.js
@@ -0,0 +1,34 @@
+const expect = require('expect');
+const supertest = require('supertest');
+
+const {app} = require('./../server');
+const {Todo} = require('./../models/todo');
+const {User} = require('./../models/user');
+
+beforeEach((done) => {
+    Todo.remove({}).then(() => {
+        done();
+    });
+});
+describe("POST /todos", () => {
+    it("Should create a new Todo", (done) => {
+        var text = 'Test Todo text';
+
+        request(app)
+            .post("/todos")
+            .send({text})
+            .expect(200)
+            .expect((res) => {
+                expect(res.body.text).toBe(text);
+            })
+            .end((err, res) => {
+                if (err) {
+                    return done(err);
+                }
+                Todo.find().then((todos) => {
+                    expect(todos.length).toBe(1);
+                    expect(todos[0].text).toBe(text);
+                }).catch((e) => done(e));
+            })
+    })
+})
