4d17dd08a4ded32f459d1d0ff80099c4b931d148
Prakhar Shukla <prakshukla@deloitte.com>
2018-05-06 20:08:13 +0530

    configuring server

---
+++ server/server.js
@@ -0,0 +1,24 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+
+var {mongoose} = require('./db/mongoose.js');
+var {User} = require('./models/user');
+var {Todo} = require('./models/todo');
+
+const app = express();
+
+app.use(bodyParser.json());
+app.post("/todos", (req, res) => {
+    var todo = new Todo({
+        text: req.body.text
+    });
+    todo.save().then((doc) => {
+        res.send(doc);
+    }, (e) => {
+        res.status(400).send(e);
+    })
+});
+
+app.listen(3000, () => {
+    console.log("Listening to port 3000");
+});
